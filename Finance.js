26aca11dd9ba9b43c763affd6d5d139ef760d38a
RilyGit <Ruslanius.fan@gmail.com>
2026-02-27 13:14:55 +0200

    The code was optimized and local storage was added.

--- Finance.js
+++ Finance.js
@@ -1,71 +1,87 @@
-const Plus_Number = document.querySelector("#plus");
-const Minus_Number = document.querySelector("#minuc");
-const Balance_User = document.querySelector("#balance"); 
-const Amount = document.querySelector("#amount");
-const Categories = document.querySelector("#categories");
+const addIncomeBtn = document.querySelector("#plus");
+const addExpenseBtn = document.querySelector("#minuc");
+const balanceDisplay = document.querySelector("#balance"); 
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#categories");
 
-const All_expenses_Create = document.querySelector("#CreateAll_expenses")
-const All_income_Create = document.querySelector("#CreateAll_income")
+const incomeContainer = document.querySelector("#CreateAll_income")
+const expensesContainer = document.querySelector("#CreateAll_expenses")
 
-const All_expenses = document.querySelector("#All_expenses")
-const All_income = document.querySelector("#All_income")
+let balance = 0;
+let history = [];
 
-let Balance = 0;
-let History = [];
+function computation(type) {
+const amount = Number(amountInput.value);
+if (!amount || amount <= 0) return;
 
-Plus_Number.addEventListener("click", function(){
-if(Amount.value > 0) {
-Balance = Balance + Number(Amount.value);
-Balance_User.textContent = Balance
-const now = new Date();
-History.push({
-  type: "plus",
-  amount: Number(Amount.value),
-  category: Categories.value,
-  time: now
-});
-}; });
-
-Minus_Number.addEventListener("click" , function(){
-if(Amount.value > 0){
-Balance = Balance - Number(Amount.value);
-Balance_User.textContent = Balance
-const now = new Date();
-History.push({
-  type: "minus",
-  amount: Number(Amount.value),
-  category: Categories.value,
-  time: now
-});
-}; });
-
-
-All_expenses.addEventListener("click" , function(){
-All_expenses_Create.innerHTML = "";
-
-History.forEach(item => {
-if(item.type === "plus"){
-const div = document.createElement("div");
-div.textContent =  `Дохід | ${item.amount} грн | ${item.category}`;
-All_expenses_Create.append(div);
+  if (type === "plus") {
+  balance += amount;
+  } else if (type === "minus") {
+  balance -= amount;
 };
-});
-});
 
-All_income.addEventListener("click" , function(){
-All_income_Create.innerHTML = "";
+balanceDisplay.textContent = balance;
 
-  History.forEach(item => {
-  if(item.type === "minus"){
-  const div = document.createElement("div");
-  div.textContent =  `Розхід | ${item.amount} грн | ${item.category}`;
-  CreateAll_income.append(div);
+    const now = new Date();
+    history.push({
+        type: type,
+        amountInput: amount,
+        category: categorySelect.value,
+        time: now
+    });
+amountInput.value = '';
+
+localStorage.setItem("historyKey", JSON.stringify(history));
+}
+function listOfHistory(){
+  incomeContainer.innerHTML = "";
+ expensesContainer.innerHTML = "";
+history.forEach(item => {
+  const div = document.createElement("div"); 
+  if (item.type === "plus") {
+    div.textContent = `Дохід | ${item.amountInput} грн | ${item.category}`;
+    incomeContainer.append(div); 
+  } else {
+    div.textContent = `Розхід | ${item.amountInput} грн | ${item.category}`;
+    expensesContainer.append(div); 
   }
 });
+};
+function updateBalance(){
+balance = 0;
+history.forEach( item => {
+if (item.type === "plus") {
+balance += item.amountInput;
+} else {
+balance -= item.amountInput;
+}
 });
+balanceDisplay.textContent = balance;
+}
+
+addIncomeBtn.addEventListener("click", function(){
+computation("plus")
+listOfHistory();
+}); 
+
+addExpenseBtn.addEventListener("click" , function(){
+computation("minus")
+listOfHistory()
+}); 
+
+const savedHistory = localStorage.getItem("historyKey");
+if (savedHistory) {
+history = JSON.parse(savedHistory);
+listOfHistory();
+updateBalance();
+};
+
+
  
 // 
-// сохранить в localStorage
+// добавлять новые категории 
+// добавить кнопку для удаление инфы с массива 
 // сделать фильтр по дате  
 // UX сделать более удобным 
+// push уведомления
 // 
